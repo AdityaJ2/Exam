02c5c13bbd3108a6a8f4d3d8ee2e031a239a3cec
AdityaJ2 <aditya.joshi@mygeorgian.ca>
2022-12-13 10:26:33 -0500

    commit update

--- src/app/components/team/team.component.ts
+++ src/app/components/team/team.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
+import { TeamService } from 'src/app/services/team.service';
 
 @Component({
   selector: 'app-team',
-  templateUrl: './team.component.html',
-  styleUrls: ['./team.component.css']
+  templateUrl: './team.component.html'
 })
-export class TeamComponent {
+export class TeamComponent implements OnInit {
+  
+  constructor(private service: TeamService) { }
 
+  teams: any
+  country: string | undefined
+  captain: string | undefined
+  primaryColour: string | undefined
+  currentRanking: number | undefined
+  
+
+  getTeams(): void {
+    this.service.getTeams().subscribe(response => {
+      this.teams = response
+    })
+   
+  }
+  clearForm(): void {
+    this.country = undefined
+    this.captain = undefined
+    this.primaryColour = undefined
+    this.currentRanking = undefined
+  }
+  
+  addTeams(): void {
+    let team = {
+      country: this.country,
+      captain: this.captain,
+      primaryColour: this.primaryColour,
+      currentRanking: this.currentRanking
+    }
+
+    this.service.addTeams(team).subscribe(response => {
+      
+      this.getTeams()
+      this.clearForm() // wipe out form input values
+      alert('Deletion Successful')
+    })
+  }
+
+  
+  selectTeams(country: string, captain: string, primaryColour: string, currentRanking: number): void {
+    this.country = country
+    this.captain = captain
+    this.primaryColour = primaryColour
+    this.currentRanking = currentRanking
+   
+  }
+
+
+
+  ngOnInit() {
+    this.getTeams()
+  }
 }
